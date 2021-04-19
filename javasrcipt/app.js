c50289ce91c22c22557de6b81b4c2923bd5d5843
YegorCreative <37757944+YegorCreative@users.noreply.github.com>
2021-04-19 11:34:29 -0500

    111

--- javasrcipt/app.js
+++ javasrcipt/app.js
@@ -1,1 +1,13 @@
-document.getElementById("demо").innerHTML = "Hey Yegor this is Awesome";
+var Yegor, Matt, Jeanne, zoom; 
+Yegor = 31;
+Matt = 34;
+Jeanne= 18;
+zoom = Yegor + Matt + Jeanne;
+
+document.getElementById("demo").innerHTML =
+"The value of z is " + zoom + ".";
+
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }
